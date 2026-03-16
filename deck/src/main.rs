d73877b1bc5bea174a8acbfb89b0ce021014865d
Jake Virdee Reynolds <jakevirdee@gmail.com>
2026-03-16 14:06:50 +0000

    chore: first-session

---
+++ deck/src/main.rs
@@ -0,0 +1,43 @@
+use rand::{rng, seq::SliceRandom};
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        let values = ["Ace", "Two", "Three"];
+
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        // Implicit return
+        Deck { cards } 
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
+}
+
+fn main() {
+    let mut deck = Deck::new();
+    deck.shuffle();
+    // Needs Error Handling
+    let cards = deck.deal(3);
+    println!("Heres your deck: {:#?}", deck);
+    println!("Heres your hand: {:#?}", cards);
+}
